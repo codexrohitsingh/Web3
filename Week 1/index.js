643dc4d06f2c0d2d358b2905826dda12e3829334
rohit singh <codex.rohit.singh@gmail.com>
2026-07-03 22:56:53 +0530

    assignment2

--- Week 1/index.js
+++ Week 1/index.js
@@ -1,3 +1,14 @@
-const crypto = require("crypto")
-const data = crypto.createHash('sha256').update("rohit").digest("hex")
-console.log(data)
+const crypto = require("crypto");
+let num = 0;
+while(true){
+
+    let input = "100xdevs"+num.toString();
+   const data = crypto.createHash("sha256").update(input).digest("hex");
+   if(data.startsWith("00000")){
+    console.log(num)
+    break;
+   }
+   else{
+    num++;
+   }
+}
